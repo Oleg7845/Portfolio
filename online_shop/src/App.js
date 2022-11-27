a9a6b8f80cd665b80ca8e8ff2ec6977e193b8120
Oleg7845 <dr.khayzenberg@gmail.com>
2022-11-27 18:50:39 +0100

    Create App.css

--- online_shop/src/App.js
+++ online_shop/src/App.js
@@ -1,9 +1,12 @@
 import React from 'react';
+import './styles/App.css';
 
 function App() {
   return (
     <div className="App">
-      
+      <header className='header'>
+
+      </header>
     </div>
   );
 }
