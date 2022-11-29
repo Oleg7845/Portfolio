27effba8a2aaf4620f1cf5491420e98c300ea60d
Oleg7845 <dr.khayzenberg@gmail.com>
2022-11-29 19:58:29 +0100

    Create Product

--- online_shop/src/App.js
+++ online_shop/src/App.js
@@ -1,23 +1,62 @@
-import React, {useState} from 'react';
+import React, {useState, useEffect} from 'react';
 import './styles/css/App.min.css';
 
 function App() {
-    const [TopBanner, setTopBannerState] = useState({
+    const [Products, setProducts] = useState({
+        id: new Date(),
+        artikel: '82NX00F6GE',
+        trademark: 'SAMSUNG',
+        title: 'Galaxy A52s 5G 128 GB Awesome Black Dual SIM',
+        price: 318,
+        description: 'SAMSUNG uses Android 11.0, One UI 3.1, KNOX 3.7 as the interface between hardware and software for this product. Do you live near the nearest big city and need fast mobile Internet? With Galaxy A52s 5G you can use the following standards: 2G (GSM), 3G (UMTS), 4G (LTE) and 5G. Thanks to the Snapdragon 778G (SM7325-2-AB) processor, the smartphone is able to perform any kind of computing productively. So you can count on the flawless operation of all functions of the device. The internal memory of the smartphone has 128 GB of free space. You can install many applications on it without using an additional SD card. To replenish the internal memory, you can insert micro SD cards into the device. This allows you to set up local capacity for photos on vacation, for example. Whether it is private or work, this phone ensures that you can complete all tasks quickly and without unnecessary wait times. Its performance is due in large part to the device is powerful 6GB of RAM. The SIM card format is Nano-SIM (4FF). When you sign up for a cell phone contract, you should ask your provider to send you a card of this size so that you can use it with this smartphone. Use the second slot for the second SIM card and switch easily between the two cards.',
+        characteristics: {
+            OS: 'Android 11.0',
+            processor: 'Snapdragon 778G (SM7325-2-AB)',
+            processor_frequency: '4x 2.4 GHz + 4x 1.8 GHz',
+            RAM: '6 GB',
+            memory: '128 GB',
+            display: {
+                diagonal: '6.5',
+                resolution: '2400 х 1080 Pixels'
+            },
+            camera: {
+                resolution: '64 Megapixel',
+                integrated_flash: 'Yes',
+                face_detection: 'Yes'
+            },
+            functions: {
+                fingerprint: 'Yes',
+                GPS_module: 'Yes',
+                Dual_SIM: 'Yes'
+            },
+            battery: {
+                type: 'Li-Ion',
+                capacity: '4500 мАч'
+            },
+        },
+        reviews: {
+            reating: 0.80,
+            title: 'Very good product',
+            text: 'So far so good, I am satisfied, recommended to do what a smartphone should do'
+        }
+    })
+
+    const [TopBanner, setTopBanner] = useState({
         text: '-70% Disconts in our OnlineShop!',
         open: true
     })
 
-    const [Sublist, setSublistState] = useState({
+    const [Sublist, setSublist] = useState({
         id: '',
         open: false
     })
     
     const closeTopBanner = ()=> {
-        setTopBannerState({text: TopBanner.text, open: false})
+        setTopBanner({text: TopBanner.text, open: false})
     }
 
     const toggleSublist = (id, open)=> {
-        setSublistState({id: id, open: open})
+        setSublist({id: id, open: open})
     }
 
     return (
